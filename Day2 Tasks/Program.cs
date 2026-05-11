5c0a47ab32c402158de6903b07c7c3a062577527
johngeorgee <johngeorgeea@gmail.com>
2026-05-11 23:39:20 +0300

    Restructure projects into separate task folders

---
+++ Day2 Tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day2_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
